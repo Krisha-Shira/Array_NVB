d29a4263b90b9b87ab59012c9a6cd33b0f0a948e
Krisha-Shira <krishashira698@gmail.com>
2025-07-22 10:29:38 +0530

    new update

---
+++ upper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+     
+        Console.Write("Enter a name: ");
+        string name = Console.ReadLine();
+
+        
+        string upperName = name.ToUpper();
+
+        
+        Console.WriteLine("Output: " + upperName);
+    }
+}
