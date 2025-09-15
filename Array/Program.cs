d009df514c2c7d01b48864842d467fb31c1f97bc
Krisha-Shira <krishashira698@gmail.com>
2025-09-15 22:12:36 +0530

    new

--- Array/Program.cs
+++ Array/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace patten_problem
+{
+    class MethodOverloadingDemo
+    {
+        void Sum(int n1, int n2)
+        {
+            Console.WriteLine("Sum of two integers: " + (n1 + n2));//sum
+        }
+        void Sum(int n1, int n2, int n3)
+        {
+            Console.WriteLine("Sum of three integers: " + (n1 + n2 + n3));
+        }
+        void Sum(double n1, double n2)
+        {
+            Console.WriteLine("Sum of two doubles: " + (n1 + n2));
+        }
+        public static void Main(String[] args)
+        {
+            MethodOverloading s1 = new MethodOverloading();
+            s1.Sum(14, 25);
+        }
+    }
+
+}
